b7e54c63518feb3a5f3817e9280176cf0243e41f
amaansayed184 <amaansayed0206@gmail.com>
2026-04-07 21:15:10 +0530

    Update MongoStore import to use default export

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const session = require("express-session");
-const MongoStore = require("connect-mongo");
+const MongoStore = require("connect-mongo").default;
 const flash = require("connect-flash");
 const ExpressError = require("./utils/ExpressError.js");
 
